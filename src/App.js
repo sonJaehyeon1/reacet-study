b7ac9746fc08ea04dae857f1b99d6aadd33571cf
son Jae hyeon <wogusdl75@naver.com>
2022-10-26 01:34:47 +0900

    3day study

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import './App.css';
-import ForceUpdate from './R026_ForceUpdate'
+import FunctionComponent from './R030_FunctionComponent'
 
 function App() {
   return (
     <div>
       <h1>Start React 200!</h1>
       <p>CSS 적용하기</p>
-      <ForceUpdate/>
+      <FunctionComponent contents = "[THIS IS FunctionComponent]"/>
     </div>
   );
 }
